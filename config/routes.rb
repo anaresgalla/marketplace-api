6723f84db02841bd99e19cdfeb38250fe18b87b9
anaresgalla <anaresgalla@gmail.com>
2024-02-28 11:10:19 -0300

    Adds session controller create action

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
                   constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: %i[show create update destroy]
+      resources :users, only: %i[index show create update destroy]
+      resources :sessions, only: %i[create destroy]
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   config.include Request::JsonHelpers, type: :controller
   config.include Request::HeadersHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.before(:each, type: :controller) do
     include_default_accept_headers
   end
