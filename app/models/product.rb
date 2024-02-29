fcf448fe09cca0be1424832868eac2487c825708
anaresgalla <anaresgalla@gmail.com>
2024-02-29 09:12:00 -0300

    Adds product model, some validations and associations

---
+++ app/models/product.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Description/Explanation of the class
+class Product < ApplicationRecord
+  belongs_to :user, optional: true
+
+  validates :title, :user_id, :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0},
+                    presence: true
+end
