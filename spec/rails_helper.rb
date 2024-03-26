2ab34590756979731442c96946e33ac167fcd6ce
anaresgalla <anaresgalla@gmail.com>
2024-03-26 15:13:41 -0300

    Adds products_spec and initial seeds

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,7 @@ RSpec.configure do |config|
     include_default_accept_headers
   end
   config.before(:each, type: :request) do |example|
-    host! 'api.example.com' 
+    host! 'api.lvh.me:3000' 
   end
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
